a9688f97a89bd37b530818f337d855c4ed3b2949
amayanij <132465809+amayanij@users.noreply.github.com>
2025-04-27 21:13:04 -0300

    Update Libro.cs

--- Libro.cs
+++ Libro.cs
@@ -17,12 +17,30 @@ namespace DSOO_Grupo14_TP1
             this.autor = autor;
             this.editorial = editorial;
         }
-
+        public string getAutor()
+        {
+            return autor;
+        }
+        public void setAutor(string autor)
+        {
+            this.autor = autor;
+        }
         public string getTitulo()
         {
             return titulo;
         }
-
+        public void setTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+         public string getEditorial()
+        {
+            return editorial;
+        }
+        public void setEditorial(string editorial)
+        {
+            this.editorial = editorial;
+        }
         public override string ToString()
         {
             return "Titulo: " + titulo + " Autor: " + autor + " Editorial: " + editorial;
